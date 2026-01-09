7383f2525313434d8828d15b24c45a0db98cd4e0
Laura Iles <40493346+janLalawa@users.noreply.github.com>
2026-01-09 15:58:40 +0000

    Initial Commit

---
+++ AudioKeepAlive/AudioKeepAliveApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioKeepAliveApp.swift
+//  AudioKeepAlive
+//
+//  Created by Vargur on 09/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct AudioKeepAliveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioKeepAliveTests/AudioKeepAliveTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudioKeepAliveTests.swift
+//  AudioKeepAliveTests
+//
+//  Created by Vargur on 09/01/2026.
+//
+
+import Testing
+@testable import AudioKeepAlive
+
+struct AudioKeepAliveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
